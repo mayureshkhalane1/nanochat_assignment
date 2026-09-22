33503a4d7a00da66831c8fccecc0c7cf2abaf3bd
BBFast <bb.kaak04@gmail.com>
2026-09-22 18:07:23 +0200

    shortened artifacts check tokenizer

--- runs/tokenizer.sh
+++ runs/tokenizer.sh
@@ -12,15 +12,15 @@ cp -r ~/.cache/nanochat/tokenizer ~/.cache/nanochat/tokenizer_8192
 python -m scripts.tok_eval
 
 # check for numbers
-python -m scripts.tok_artifacts --text "There live 1,586,291 people in this city in 2025. An average of 20.13 people per kilometer with an age of 58."
+python -m scripts.tok_artifacts --text "1,586,291 people live in country 224393."
 
 # check for source code
 python -m scripts.tok_artifacts --text "
 def hello_world():
     print('Hello, world!')
 hello_world()"
+
 # check for non-english text
-python -m scripts.tok_artifacts --text "Bonjour, comment allez-vous?"
 python -m scripts.tok_artifacts --text "Hola, ¿cómo estás?"
 python -m scripts.tok_artifacts --text "정직한 사실 위에, 공정한 시선을 더하다"
 
@@ -34,7 +34,7 @@ python -m scripts.tok_eval
 
 
 # check for numbers
-python -m scripts.tok_artifacts --text "There live 1,586,291 people in this city in 2025. An average of 20.13 people per kilometer with an age of 58."
+python -m scripts.tok_artifacts --text "1,586,291 people live in country 224393."
 
 # check for source code
 python -m scripts.tok_artifacts --text "
@@ -43,6 +43,5 @@ def hello_world():
 hello_world()"
 
 # check for non-english text
-python -m scripts.tok_artifacts --text "Bonjour, comment allez-vous?"
 python -m scripts.tok_artifacts --text "Hola, ¿cómo estás?"
 python -m scripts.tok_artifacts --text "정직한 사실 위에, 공정한 시선을 더하다"
